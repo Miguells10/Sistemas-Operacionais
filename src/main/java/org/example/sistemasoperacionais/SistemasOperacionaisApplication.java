f21e08e83434bc77ae958f28787869fe2e9b2f2d
Miguel Lucas <lucasmiguel646@gmail.com>
2025-07-03 21:44:23 -0300

    create project

---
+++ src/main/java/org/example/sistemasoperacionais/SistemasOperacionaisApplication.java
@@ -0,0 +1,13 @@
+package org.example.sistemasoperacionais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasOperacionaisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemasOperacionaisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sistemasoperacionais/SistemasOperacionaisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sistemasoperacionais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasOperacionaisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
