5b5e569e1f006f30f05e7d9dfa338ea0f3666830
Miguel Lucas <lucasmiguel646@gmail.com>
2025-07-03 23:15:00 -0300

    Implement sleeping barber problem simulation
    Add initial implementation of the sleeping barber problem using Java concurrency primitives. Includes Barbeiro (barber) and Cliente (customer) classes, a Salao (barbershop) to manage the queue and threads, and a Main class to start the simulation.

---
+++ src/main/java/org/example/sistemasoperacionais/barbeiroDorminhoco/Cliente.java
@@ -0,0 +1,18 @@
+package org.example.sistemasoperacionais.barbeiroDorminhoco;
+
+public class Cliente {
+    private Integer id;
+
+    public Cliente(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
+
